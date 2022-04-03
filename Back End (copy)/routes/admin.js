d743149fba497797061b8b5e006e8c776b4cb793
Darpit Makwana <79750384+TechDarpit@users.noreply.github.com>
2022-04-03 14:50:47 +0530

    Delete Back End (copy) directory

--- Back End (copy)/routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const adminController = require("../controller/admin");
-
-const router = express.Router();
-
-router.post("/signin", adminController.signIn);
-
-module.exports = router;
--- Back End (copy)/util/db.js
+++
@@ -1,8 +0,0 @@
-// const { Sequelize } = require("sequelize");
-
-// const sequelize = new Sequelize("Admin_System", "root", "Root@123456", {
-//   dialect: "mysql",
-//   host: "localhost",
-// });
-
-// module.exports = sequelize;
