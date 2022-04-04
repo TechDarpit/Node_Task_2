cfd4ce55aac5c7e6c3e1fa82bf40eb51a5c64ef4
Darpit Makwana <79750384+TechDarpit@users.noreply.github.com>
2022-04-04 18:05:15 +0530

    Delete Front End (copy) directory

--- Front End (copy)/helper/date.ts
+++
@@ -1,8 +0,0 @@
-export const dateConvert = (date: Date) => {
-  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
-    day: "numeric",
-    month: "short",
-    year: "numeric",
-  });
-  return humanReadableDate;
-};
